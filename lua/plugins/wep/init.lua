71fceace9bc38c35fd6bdaeac3a02ff10bdb4c7b
Cristian31310 <cristian.martin.almendros@gmail.com>
2025-11-23 00:37:41 +0100

    install nvim-autotag

--- lua/plugins/wep/init.lua
+++ lua/plugins/wep/init.lua
@@ -13,4 +13,18 @@ return {
   {
     "norcalli/nvim-colorizer.lua",
   },
+  {
+    "windwp/nvim-ts-autotag",
+    opts = {
+      enable_close = true, -- Auto close tags
+      enable_rename = true, -- Auto rename pairs of tags
+      enable_close_on_slash = false,
+    },
+
+    per_filetype = {
+      ["html"] = {
+        enable_close = false,
+      },
+    },
+  },
 }
